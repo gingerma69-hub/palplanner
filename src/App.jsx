4817bec5b0488040e37774e872fa27a425a4b579
Marah <gingerma69@hotmail.com>
2026-07-01 01:17:34 -0400

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Palworld Base Planner</h1>
+
+      <p>Version 0.1</p>
+
+      <p>Created by Marah</p>
+    </div>
+  );
+}
+
+export default App;
